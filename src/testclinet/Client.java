a48a763f7cee4e381fb7b4dd0b33dc356b7a6af6
nicolai1233 <nicolai1233@users.noreply.github.com>
2018-10-24 10:47:13 +0200

    client update

--- src/testclinet/Client.java
+++ src/testclinet/Client.java
@@ -11,24 +11,36 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
 
-    public static void main(String args[]) {
+    private BufferedReader stdIn;
+    private PrintWriter out;
+    private BufferedReader in;
+    
+    public static void main(String[] args) throws IOException {
         String host = "127.0.0.1";
         int port = 8081;
-        new Client(host, port);
+        Client c = new Client(host, port);
+        Scanner scan  = new Scanner(System.in);
+      while(true){
+          String a  = scan.nextLine();
+        System.out.println(c.test(a));
+      }
     }
 
     public Client(String host, int port) {
+        
+        
         try {
             String serverHostname = new String("127.0.0.1");
 
             System.out.println("Connecting to host " + serverHostname + " on port " + port + ".");
 
             Socket echoSocket = null;
-            PrintWriter out = null;
-            BufferedReader in = null;
+            out = null;
+            in = null;
 
             try {
                 echoSocket = new Socket(serverHostname, 8081);
@@ -43,26 +55,35 @@ public class Client {
             }
 
             /** {@link UnknownHost} object used to read from console */
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            stdIn = new BufferedReader(new InputStreamReader(System.in));
 
-            while (true) {
-                System.out.print("client: ");
-                String userInput = stdIn.readLine();
-                /** Exit on 'q' char sent */
-                if ("q".equals(userInput)) {
-                    break;
-                }
-                out.println(userInput);
-                System.out.println("server: " + in.readLine());
-            }
+          //  test();
 
             /** Closing all the resources */
-            out.close();
-            in.close();
-            stdIn.close();
-            echoSocket.close();
+//            out.close();
+//            in.close();
+//            stdIn.close();
+//            echoSocket.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    public String test(String i) throws IOException{
+      //  while (true) {
+                String client = "client: ";
+                //System.out.print("client: ");
+               // String userInput = stdIn.readLine();
+                /** Exit on 'q' char sent */
+//                if ("q".equals(userInput)) {
+//                    break;
+//                }
+                out.println(i);
+                String clientOut = client+" "+i;
+                String server = "server"+in.readLine();
+                return clientOut+"\n"+server; 
+               // System.out.println("server: " + in.readLine());
+            //} 
+       // return null;
     }
 }
