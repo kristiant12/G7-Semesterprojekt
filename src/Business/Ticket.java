3342c0e89023db35cc5aad2e4ede5cf2a02a620d
jazzmano <32385275+jazzmano@users.noreply.github.com>
2018-10-31 10:26:41 +0100

    Ticket classes (ITICKET, Ticket)

---
+++ src/Business/Ticket.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import Acquaintance.ITicket;
+
+/**
+ *
+ * @author rober
+ */
+public class Ticket implements ITicket {
+    
+    private String issueNumber;
+    private String issueDescription;
+
+    
+    public Ticket(){
+        
+    }
+
+    @Override
+    public String getIssuenumber() {
+      return issueNumber;
+    }
+
+    @Override
+    public void setIssuenumber(String issueNumber) {
+       this.issueNumber = issueNumber;
+
+    }
+
+    @Override
+    public String getIssueDescription() {
+         return issueDescription;
+    }
+
+    @Override
+    public void setIssueDescription(String issueDescription) {
+         this.issueDescription = issueDescription;
+
+    }
+    public String toString(){
+          return issueNumber + issueDescription;
+
+    }
+    
+}
