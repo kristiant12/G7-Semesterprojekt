9ecb654efcddc4e5bef34a1546b7b0584860b46e
jazzmano <32385275+jazzmano@users.noreply.github.com>
2018-10-31 10:27:07 +0100

    Update src/Acquaintance/ITicket.java
    Ticket

---
+++ src/Acquaintance/ITicket.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Acquaintance;
+
+/**
+ *
+ * @author rober
+ */
+public interface ITicket {
+    
+   
+    
+    public String getIssuenumber();
+    
+    public void setIssuenumber(String issueNumber);
+   
+    public String getIssueDescription();
+    
+    public void setIssueDescription(String issueDescription);
+    
+    
+    
+}
