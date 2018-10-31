07d60b1df1a0fa89d0ce095685d3221f0206b39f
AC-98 <AC-98@users.noreply.github.com>
2018-10-31 10:58:45 +0100

    GUI bid

--- src/GUI/BidController.java
+++ src/GUI/BidController.java
@@ -5,14 +5,20 @@
  */
 package GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -43,9 +49,20 @@ public class BidController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+        private void changeScreen(MouseEvent event, String a) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource(a));
+        Scene screen = new Scene(parent);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(screen);
+        window.show();
+    }
 
     @FXML
-    private void bidBackArrowClicked(MouseEvent event) {
+    private void bidBackArrowClicked(MouseEvent event) throws IOException {
+        changeScreen(event, "Manufacturer.fxml");
     }
     
 }
