0110873ef35297528281f9845305d38ae73e3079
AC-98 <AC-98@users.noreply.github.com>
2018-10-31 11:44:42 +0100

    GUI Support/Ticket

--- src/GUI/SupportController.java
+++ src/GUI/SupportController.java
@@ -5,14 +5,20 @@
  */
 package GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -33,9 +39,20 @@ public class SupportController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+         private void changeScreen(MouseEvent event, String a) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource(a));
+        Scene screen = new Scene(parent);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(screen);
+        window.show();
+    }
 
     @FXML
-    private void supportBackArrowClicked(MouseEvent event) {
+    private void supportBackArrowClicked(MouseEvent event) throws IOException {
+        changeScreen(event, "Customer.fxml");
     }
 
     @FXML
--- src/GUI/TicketController.java
+++ src/GUI/TicketController.java
@@ -5,16 +5,22 @@
  */
 package GUI;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -39,9 +45,20 @@ public class TicketController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+        private void changeScreen(MouseEvent event, String a) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource(a));
+        Scene screen = new Scene(parent);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(screen);
+        window.show();
+    }
 
     @FXML
-    private void ticketBackArrowClicked(MouseEvent event) {
+    private void ticketBackArrowClicked(MouseEvent event) throws IOException {
+        changeScreen(event, "Support.fxml");
     }
 
     @FXML
