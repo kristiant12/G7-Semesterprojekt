61f7631b4ab362ab0ab0e260f14e007e7a8ae006
jazzmano <32385275+jazzmano@users.noreply.github.com>
2018-12-12 21:15:45 +0100

    helvedes meget javadoc

--- src/Business/BusinessFacade.java
+++ src/Business/BusinessFacade.java
@@ -35,7 +35,11 @@ public class BusinessFacade implements IBusiness{
         private Case newCase;
         private Manufacturer newManufactor;
         private Ticket ticket;
-        public BusinessFacade() {
+
+    /**
+     *
+     */
+    public BusinessFacade() {
         this.client = new Client();
         //user = null;
         
@@ -51,6 +55,12 @@ public class BusinessFacade implements IBusiness{
 //    }
     
        @Override
+       
+       /**
+        * Creates a case, which has the appropriate parameters
+        */
+       
+     
      public void createCase(String caseTitle, String caseID,String caseBudget,String deadline, String component,boolean evaluated,String freeText) throws IOException{
         Case SendCase = new Case(caseTitle, caseID,caseBudget,deadline,component,evaluated,freeText);
            setCase((ICase)SendCase);
@@ -58,11 +68,17 @@ public class BusinessFacade implements IBusiness{
      }
      
        @Override
+       /**
+        * gets the case
+        */
      public Case getCase(){
          return newCase;
      }
      
        @Override
+       
+      
+       
      public void setCase(ICase ia){
          Case c = (Case) ia;
          this.newCase = c;
@@ -98,6 +114,10 @@ public class BusinessFacade implements IBusiness{
     }
     
         @Override
+        
+        /**
+         * Gets the user
+         */
     public User getUser(){
 //        if(user instanceof Manufacturer){
 //            return (Manufacturer) user;
@@ -124,7 +144,16 @@ public class BusinessFacade implements IBusiness{
     public Customer getCustumer(){
         return (Customer) user;
     }
-            
+          
+    
+    /**
+     * This method checks whether password and username matches, done with an if--statement.
+     * @param name name of user
+     * @param password password of user
+     * @return returns the user
+     * @throws ClassNotFoundException thrown when class is not found
+     * @throws IOException  thrown when IOException occurs.
+     */
     public User login(String name, String password) throws ClassNotFoundException, IOException{
         List<User> list = client.getUserFromServer();
         for (int i = 0; i < list.size(); i++) {
@@ -139,12 +168,24 @@ public class BusinessFacade implements IBusiness{
         return null;
     }
 
+    
     @Override
     public List<Case> getCaseFromServer() throws IOException, ClassNotFoundException {
        return client.getCaseFromServer();
     }
     
 
+    /**
+     * Modifies a case with the editcase() method
+     * @param caseTitle title of a case
+     * @param caseID ID of a case
+     * @param caseBudget Budget of a case
+     * @param deadline deadline of  a case
+     * @param component componenent of the case
+     * @param evaluated boolean value to state whether the case has been evaluated or not.
+     * @param freeText freetext which is the description of the case
+     * @throws IOException thrown when IOException. 
+     */
        @Override
     public void modifyCase(String caseTitle, String caseID, String caseBudget, String deadline, String component, boolean evaluated, String freeText) throws IOException{
       
@@ -157,18 +198,33 @@ public class BusinessFacade implements IBusiness{
 //    
     
     
-    // denne metode sender en ny user til sql
+    /**
+     * sends a new user to the sql
+     * @param user, a user
+     * @throws IOException thrown when IOException 
+     */
     @Override
-    public void sendUser(User d) throws IOException {
-        client.sendUser(d);
+    public void sendUser(User user) throws IOException {
+        client.sendUser(user);
     }
 
+    
+    /**
+     * Sends a new case to the sql 
+     * @param casee is the case
+     * @throws IOException  catches IOException
+     */
     @Override
-    public void sendCase(Case a) throws IOException {
-        client.sendCase(a);
+    public void sendCase(Case casee) throws IOException {
+        client.sendCase(casee);
     }
     
     
+    /**
+     * Deletes a case in sql
+     * @param ic is an interface Case
+     * @throws IOException 
+     */
     
        @Override
     public void deleteCase(ICase ic) throws IOException{
@@ -177,78 +233,147 @@ public class BusinessFacade implements IBusiness{
     }
 
     /**
-     *
-     * @param ticket
-     * @throws IOException
+     *sends ticket to the sql 
+     * @param ticket is a ticket from the ticket class
+     * @throws IOException thrown when IOException
      */
     @Override
     public void sendTicket(Ticket ticket) throws IOException {
         client.sendTicket(ticket);
     }
+    /**
+     * sends a map of the Usersn, and the cases through sql
+     * @param casee is a case parameter
+     * @throws IOException thrown when there is an IOException. 
+     */
 
     @Override
-    public void sendMapOfUserAndCases(Case b) throws IOException {
-        client.sendMapOfUserAndCases(getUser(), b);
+    public void sendMapOfUserAndCases(Case casee) throws IOException {
+        client.sendMapOfUserAndCases(getUser(), casee);
     }
+    /**
+     * gets the user case list
+     * @param user is a User parameter from the user class
+     * @return return a list of the users, through the client
+     * @throws IOException thrown when there appears an IOException
+     * @throws ClassNotFoundException  is thrown when there is a classnotfoundexception.
+     */
 
     @Override
-    public List<Case> getUserCaseList(User a) throws IOException, ClassNotFoundException {
-        return client.getUserCaseList(a);
+    public List<Case> getUserCaseList(User user) throws IOException, ClassNotFoundException {
+        return client.getUserCaseList(user);
     }
     
+    /**
+     * 
+     * @param customer is a custoemr
+     * @returnreturns a list of user Cases
+     * @throws IOException is thrown when there is a IOException
+     * @throws ClassNotFoundException thrown when there is a classnotfoundexception. 
+     */
    
        @Override
-    public List<Case> getUserCaseList2(Customer a) throws IOException, ClassNotFoundException {
+    public List<Case> getUserCaseList2(Customer customer) throws IOException, ClassNotFoundException {
        // Customer c = (Customer) a;
-        return client.getUserCaseList1(a);
+        return client.getUserCaseList1(customer);
     }
     
-    @Override
-    public void SendPicture(Picture pic) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
+    
+    //SLETTES?
+//    @Override
+//    public void SendPicture(Picture pic) {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
+//
+//    @Override
+//    public List<Picture> getPictureFromServer() throws IOException, ClassNotFoundException {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+//    }
 
-    @Override
-    public List<Picture> getPictureFromServer() throws IOException, ClassNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    
+    
+    
+    /**
+     *
+     * @returns a list with the not evaluated cases
+     * @throws IOException thrown when IO exception
+     * @throws ClassNotFoundException thrown when classnotfoundexception
+     */
     @Override
     public List<Case> getNotEvaluatetCase() throws IOException, ClassNotFoundException {
        return client.getNotEvaluatetCase();
     }
 
+    /**
+     * 
+     * @param ic is the Icase
+     * @throws IOException thrown when there is an IOException
+     */
     @Override
     public void Evaluate(ICase ic) throws IOException {
         Case c = (Case) ic;
         client.Evaluate(c);
     }
+    /**
+     * 
+     * @param lastKnownCity is the last known city the 
+     * @param estimatedTime
+     * @param FormOfTransportation
+     * @param cs
+     * @return 
+     */
     public Logistics updateLogistics(String lastKnownCity, int estimatedTime, String FormOfTransportation, Case cs){
-        
-        
-        
-           return null;
+    
+        return null;
     }
     
-    public void trackLogistics(){
-        
-    }
+    //Slettes?
+//    public void trackLogistics(){
+//        
+//    }
 
+    
+    /**
+     * 
+     * @return a list of tickets from the server
+     * @throws IOException thrown when a IOException occurs
+     * @throws ClassNotFoundException thrown when a classfoundexception occurs
+     */
     @Override
     public List<Ticket> getTicketFromServer() throws IOException, ClassNotFoundException {
         return client.getTicketFromServer();
     }
+    
+    /**
+     * 
+     * @returns a list with the evaluated cases
+     * @throws IOException thrown when a IOException occurs
+     * @throws ClassNotFoundException thrown when a classnotfoundexception occurs
+     */
 
     @Override
     public List<Case> getEvaluatetList() throws IOException, ClassNotFoundException {
         return client.getEvaluatetList();
     }
+    /**
+     * deletes a uses from the sql. 
+     * @param iu is a Iuser from the IUser interface
+     * @throws IOException thrown when a IOException occurs
+     */
 
     @Override
     public void deleteUser(IUser iu) throws IOException {
         User u = (User) iu;
         client.deleteUser(u);
     }
+    /**
+     * creates a ticket using sql queries
+     * @param issueNumber is the issuenumber regarding a ticket
+     * @param issueDescript is the issue description regarding a ticket
+     * @param ic is a parameter from the custoemr class
+     * @throws IOException  thrown when a IOException occurs
+     */
 
     @Override
     public void createTicket(String issueNumber,String issueDescript, Customer ic) throws IOException {
@@ -257,36 +382,70 @@ public class BusinessFacade implements IBusiness{
         client.createTicket(ticket2, ic);
     }
     
+    /**
+     * Sets a ticket
+     * @param ticket variable from ticket class
+     */
     public void setTicket(Ticket ticket){
         this.ticket = ticket;
     }
     
+    /**
+     * gets  ticket
+     * @return ticket
+     */
     public Ticket getTicket(){
         return ticket;
     }
+    /**
+     * gets teh tickets specific to a customer
+     * @param ic custoemer parameter
+     * @returns a list with the cases relevant
+     * @throws IOException thrown when a IOException occurs
+     * @throws ClassNotFoundException thrown when a Classnotfoundexception occurs
+     */
 
     @Override
     public List<Ticket> getAllSpecifikCustumerTicket(Customer ic) throws IOException, ClassNotFoundException {
-        
-        
+               
         return client.getAllSpecifikCustumerTicket(ic);
     }
 
+    /**
+     * 
+     * @param m manufacturer parameter
+     * @throws IOException thrown when a IOException occurs
+     */
     @Override
     public void updateManufactor(Manufacturer m) throws IOException {
         client.updateManufactor(m);
     }
 
-   
+    
+    /**
+     * Creates a manufactuer
+     * @param username username for the manufactuer
+     * @param password password for the manufacturer
+     * @throws IOException  thrown when a IOException occurs
+     */
     public void createManufactur(String username,String password) throws IOException{
         Manufacturer a = new Manufacturer(username, password, null, 0, null, null);
         sendUser(a);
         
     }
+    /**
+     * sets manufacturer
+     * @param m manufacturer parameter
+     */
     
     public void setManufactor(Manufacturer m){
         this.newManufactor = m;
     }
+    /**
+     * 1
+     * @param it parameter from ITicket interface
+     * @throws IOException thrown when a IOException occurs
+     */
 
     @Override
     public void employeeReplyTekst(ITicket it) throws IOException {
@@ -294,6 +453,12 @@ public class BusinessFacade implements IBusiness{
         client.employeeReplyTekst(t);
     }
     
+    /**
+     * registers a bid in the database
+     * @param ic parameter from the ICase interface
+     * @param bid what the bid is on
+     * @throws IOException thrown when a IOException occurs
+     */
        @Override
     public void registerBid(ICase ic,double bid) throws IOException{
         Case c = (Case) ic;
@@ -301,11 +466,27 @@ public class BusinessFacade implements IBusiness{
         client.RegisterBid(c);
     }
 
+    /**
+     * gets auction
+     * @return list of  cases 
+     * @throws IOException thrown when a IOException occurs
+     * @throws ClassNotFoundException thrown when a classnotfoundexception occurs
+     */
     @Override
     public List<Case> getAuction() throws IOException, ClassNotFoundException {
         return client.getAuction();
         
     }
+    /**
+     * creates a  manufacturer with his parameters
+     * @param pass manufacturers pass
+     * @param userNam manufacturers username
+     * @param firmaddress manufacturers address
+     * @param number his number
+     * @param firmaName his firm name 
+     * @param firmaMail his firm email
+     * @return a manufacturer
+     */
     
        @Override
     public Manufacturer createManufactor(String pass, String userNam,String firmaddress,int number,String firmaName,String firmaMail){
