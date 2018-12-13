80691166d0faa025da990ef739306bd36433950c
jazzmano <32385275+jazzmano@users.noreply.github.com>
2018-12-13 10:20:14 +0100

    Merge origin/master
    Conflicts:
    	nbproject/private/private.xml

--- src/Business/Auction.java
+++ src/Business/Auction.java
@@ -14,17 +14,33 @@ import java.util.List;
  */
 public class Auction {
     
+    /**
+     * List of cases
+     */
     private List<Case> list;
     
+    
+    /**
+     * Constructor which initiates the list into an arraylist
+     */
     public Auction(){
         list = new ArrayList<>();
     }
 
+    /**
+     * Inserts a case through sql query
+     * @param newCase parameter from the case class
+     */
     public void insertCase(Case newCase){
         list.add(newCase);
     }
     
-
+    
+    /**
+     * Registers a bid by setting it
+     * @param cs Case class paramter
+     * @param bid a double number to represent what has been bid 
+     */
     public void registerBid(Case cs, double bid) {
         cs.setBid(bid);
     }
